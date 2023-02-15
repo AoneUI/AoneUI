e37e0a7838a3143b640780cb714f7a3a0c059323
杉菜 <lf395849@digital-engine.com>
2023-02-15 19:13:18 +0800

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, UserConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export const config=({
+  plugins: [
+    react()
+  ],
+  build: {
+    minify: 'terser', // boolean | 'terser' | 'esbuild'
+    sourcemap: true, // 输出单独 source文件
+    cssCodeSplit: true,
+    lib: {
+      entry: "./src/entry.ts",
+      name: 'React-UI-Teaching',
+      formats: ["esm", "umd","iife"],
+      fileName: 'react-ui-teaching',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React'
+        },
+        extend: true,
+        assetFileNames:()=> 'style.css'
+      }
+    },
+    outDir: "./dist"
+  }
+})
+export default defineConfig(config as UserConfig);
