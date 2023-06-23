3215fbc3b027c177e221cdb3ded99bfa408ac9fd
杉菜 酱子_ <fang1846015350@163.com>
2023-06-23 09:53:08 +0000

    fix: code style and autogenerate code

--- src/Icon/props.ts
+++
@@ -1,9 +0,0 @@
-interface iconProps {
-    iconName: string | undefined;
-    customClassName?: string;
-    onIconClick?: () => void;
-}
-
-export {
-    iconProps
-}
---
+++ src/button/demo/index.tsx
@@ -0,0 +1,14 @@
+import { AppBar, Button, Icon } from 'aone-ui';
+import React from 'react';
+
+const ButtonDemo = () => (
+  return (
+    <div>
+      <AppBar title="基本使用">
+        // TODO
+      </AppBar>
+    </div>
+  );
+);
+
+export default ButtonDemo;
