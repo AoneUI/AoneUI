6f6304fc43fe50046a63a0cfe32ccf958c5043b0
coder-fang <1846015350@qq.com>
2023-06-27 00:26:43 +0800

    feat(ui/button): button component ✨✨✨

--- src/button/props.ts
+++ src/button/props.ts
@@ -1,1 +1,31 @@
-export const props = {}
+export type ButtonProps = {
+  block?: boolean;
+  text?: boolean;
+  outline?: boolean;
+  color?: string;
+  textColor?: string;
+  type?: 'default' | 'primary' | 'info' | 'success' | 'warning' | 'danger';
+  nativeType?: 'button' | 'reset' | 'submit';
+  size?: 'normal' | 'mini' | 'small' | 'large';
+  loading?: boolean;
+  round?: boolean;
+  disabled?: boolean;
+  ripple?: boolean;
+  autoLoading?: boolean;
+  children?: React.ReactNode;
+  onTouchstart?: (event: React.MouseEvent) => void;
+  onClick?: (event: React.MouseEvent) => void;
+};
+
+export const buttonDefaultProps: ButtonProps = {
+  type: 'default',
+  nativeType: 'button',
+  size: 'normal',
+  loading: false,
+  round: false,
+  block: false,
+  text: false,
+  outline: false,
+  disabled: false,
+  ripple: false
+};
